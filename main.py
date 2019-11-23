a8a385a9f1c4dd066b4bfc0c8c5efb66c2525fec
不肖生 <xinkucheng@163.com>
2019-11-23 15:25:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-execute(['scrapy', 'crawl', 'crawler'])
+# execute(['scrapy', 'crawl', 'crawler'])
+# execute(['scrapy', 'crawl', 'topic_creators'])
+execute(['scrapy', 'crawl', 'topicitems'])
